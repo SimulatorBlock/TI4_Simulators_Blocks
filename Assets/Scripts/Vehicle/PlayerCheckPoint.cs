4afc4cb96a7f995065c6079714fac84103035a7e
Rafael_Madureira <rafinhakd4@gmail.com>
2022-09-04 22:36:44 -0300

    Faze para amostra

--- Assets/Scripts/Vehicle/PlayerCheckPoint.cs
+++ Assets/Scripts/Vehicle/PlayerCheckPoint.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class PlayerCheckPoint : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] List<GameObject> checkPoints;
-    [SerializeField] Vector3 vectorPoint;
-    private int checkpointCount = 1;
+    //[SerializeField] GameObject player;
+    //[SerializeField] List<GameObject> checkPoints;
+    //[SerializeField] Vector3 vectorPoint;
+    //private int checkpointCount = 1;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            player.transform.position = vectorPoint;
-            this.gameObject.transform.rotation = Quaternion.Euler(0,0,0);
-            this.gameObject.GetComponent<Rigidbody>().Sleep();
-        }
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.R))
+    //    {
+    //        player.transform.position = vectorPoint;
+    //        this.gameObject.transform.rotation = Quaternion.Euler(0,0,0);
+    //        this.gameObject.GetComponent<Rigidbody>().Sleep();
+    //    }
 
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        vectorPoint=player.transform.position;
-        Destroy(other.gameObject);
-    }
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    vectorPoint=player.transform.position;
+    //    Destroy(other.gameObject);
+    //}
 
 }
