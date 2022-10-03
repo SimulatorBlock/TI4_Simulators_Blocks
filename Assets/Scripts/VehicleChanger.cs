f5201b0a02ba991a9692d742632983add2d448ca
Rafael_Madureira <rafinhakd4@gmail.com>
2022-10-03 13:37:31 -0300

    Adição de carros

--- Assets/Scripts/VehicleChanger.cs
+++ Assets/Scripts/VehicleChanger.cs
@@ -21,6 +21,20 @@ public class VehicleChanger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha6)) InstantiateVehicle(5);
         if (Input.GetKeyDown(KeyCode.Alpha7)) InstantiateVehicle(6);
         if (Input.GetKeyDown(KeyCode.Alpha8)) InstantiateVehicle(7);
+        if (Input.GetKeyDown(KeyCode.Alpha9)) InstantiateVehicle(8);
+        if (Input.GetKeyDown(KeyCode.Alpha0)) InstantiateVehicle(9);
+        if (Input.GetKeyDown(KeyCode.Q)) InstantiateVehicle(10);
+        if (Input.GetKeyDown(KeyCode.W)) InstantiateVehicle(11);
+        if (Input.GetKeyDown(KeyCode.E)) InstantiateVehicle(12);
+        if (Input.GetKeyDown(KeyCode.R)) InstantiateVehicle(13);
+        if (Input.GetKeyDown(KeyCode.T)) InstantiateVehicle(14);
+        if (Input.GetKeyDown(KeyCode.Y)) InstantiateVehicle(15);
+        if (Input.GetKeyDown(KeyCode.U)) InstantiateVehicle(16);
+        if (Input.GetKeyDown(KeyCode.I)) InstantiateVehicle(17);
+        if (Input.GetKeyDown(KeyCode.O)) InstantiateVehicle(18);
+        if (Input.GetKeyDown(KeyCode.P)) InstantiateVehicle(19);
+        if (Input.GetKeyDown(KeyCode.A)) InstantiateVehicle(20);
+        if (Input.GetKeyDown(KeyCode.S)) InstantiateVehicle(21);
     }
 
     private void InstantiateVehicle(int vehicleId)
