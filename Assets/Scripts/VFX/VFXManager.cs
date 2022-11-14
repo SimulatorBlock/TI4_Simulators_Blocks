9e460208b2c0344ec9d75c29d0cd87d0f1ee1b2d
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2022-11-14 15:25:22 -0300

    VFX manager e SaveGame

---
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct VFXs
+{
+    public string name;
+    public GameObject vfxGameObj;
+    public ParticleSystem vfxParticleSys;
+}
+public class VFXManager : MonoBehaviour
+{
+    public static VFXManager Instance;
+
+    private void Awake()
+    {
+        if(Instance) {Destroy(gameObject); return;}
+        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField] private VFXs[] vfXsList;
+    
+    
+    //Metodos para ativar/desativar
+    //Metodos para mudar propriedades e tempo seila..
+
+    public void EnableVFX(string vfxName)
+    {
+        VFXs vfx = Array.Find(vfXsList, vFXs => vFXs.name == vfxName);
+        
+        vfx.vfxGameObj.SetActive(true);
+        //vfx.vfxParticleSys.seila;
+    }
+}
