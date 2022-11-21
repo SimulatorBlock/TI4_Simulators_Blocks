caaf20004d0aef16e6ebc7f646e040b4ee3e48f7
Rimec <renan.rm31@gmail.com>
2022-11-20 22:23:33 -0300

    fix: botoes

--- Assets/Scripts/Canvas/CanvasManagarEdit.cs
+++ Assets/Scripts/Canvas/CanvasManagarEdit.cs
@@ -7,34 +7,34 @@ using AutoPilot;
 
 public class CanvasManagarEdit : MonoBehaviour
 {
-    // [Header("Defaults")]
-    // [SerializeField] private Button standardButton;
-    // [SerializeField] private Button engineButton;
-    // [SerializeField] private Button wheelButton;
-    // [SerializeField] private Button removeButton;
-    // [SerializeField] private Button playButton;
-    // [SerializeField] private Button clearVehicleButton;
-    // [Space(10)]
+    [Header("Defaults")]
+    [SerializeField] private Button standardButton;
+    [SerializeField] private Button engineButton;
+    [SerializeField] private Button wheelButton;
+    [SerializeField] private Button removeButton;
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button clearVehicleButton;
+    [Space(10)]
 
-    // [Header("Standard")]
-    // [SerializeField] private Button standardLevel1Button;
-    // [SerializeField] private Button standardLevel2Button;
-    // [SerializeField] private Button standardLevel3Button;
-    // [Space(10)]
+    [Header("Standard")]
+    [SerializeField] private Button standardLevel1Button;
+    [SerializeField] private Button standardLevel2Button;
+    [SerializeField] private Button standardLevel3Button;
+    [Space(10)]
     
-    // [Header("Engine")]
-    // [SerializeField] private Button engineLevel1Button;
-    // [SerializeField] private Button engineLevel2Button;
-    // [SerializeField] private Button engineLevel3Button;
-    // [Space(10)]
+    [Header("Engine")]
+    [SerializeField] private Button engineLevel1Button;
+    [SerializeField] private Button engineLevel2Button;
+    [SerializeField] private Button engineLevel3Button;
+    [Space(10)]
     
-    // [Header("Wheel")]
-    // [SerializeField] private Button wheelSimpleButton;
-    // [SerializeField] private Button wheelSimpleBackButton;
-    // [SerializeField] private Button wheelSimpleFrontButton;
-    // [SerializeField] private Button wheelTopButton;
-    // [SerializeField] private Button wheelTopBackButton;
-    // [SerializeField] private Button wheelTopFrontButton;
+    [Header("Wheel")]
+    [SerializeField] private Button wheelSimpleButton;
+    [SerializeField] private Button wheelSimpleBackButton;
+    [SerializeField] private Button wheelSimpleFrontButton;
+    [SerializeField] private Button wheelTopButton;
+    [SerializeField] private Button wheelTopBackButton;
+    [SerializeField] private Button wheelTopFrontButton;
     // [Space(10)]
     
     // [Header("Empty's")]
@@ -54,36 +54,36 @@ public class CanvasManagarEdit : MonoBehaviour
         }
     }
 
-    // void Start()
-    // {
-    //     #region  Standard
-    //     standardButton.onClick.AddListener(ClickStandardButton);
-    //     standardLevel1Button.onClick.AddListener(ClickStandardLevel1Button);
-    //     standardLevel2Button.onClick.AddListener(ClickStandardLevel2Button);
-    //     standardLevel3Button.onClick.AddListener(ClickStandardLevel3Button);
-    //     #endregion
+    void Start()
+    {
+        #region  Standard
+        standardButton.onClick.AddListener(ClickStandardButton);
+        standardLevel1Button.onClick.AddListener(ClickStandardLevel1Button);
+        standardLevel2Button.onClick.AddListener(ClickStandardLevel2Button);
+        standardLevel3Button.onClick.AddListener(ClickStandardLevel3Button);
+        #endregion
 
-    //     #region Engine
-    //     engineButton.onClick.AddListener(ClickEngineButton);
-    //     engineLevel1Button.onClick.AddListener(ClickEngineLevel1Button);
-    //     engineLevel2Button.onClick.AddListener(ClickEngineLevel2Button);
-    //     engineLevel3Button.onClick.AddListener(ClickEngineLevel3Button);
-    //     #endregion
+        #region Engine
+        engineButton.onClick.AddListener(ClickEngineButton);
+        engineLevel1Button.onClick.AddListener(ClickEngineLevel1Button);
+        engineLevel2Button.onClick.AddListener(ClickEngineLevel2Button);
+        engineLevel3Button.onClick.AddListener(ClickEngineLevel3Button);
+        #endregion
 
-    //     #region Wheel
-    //     wheelButton.onClick.AddListener(ClickWheelButton);
-    //     wheelSimpleButton.onClick.AddListener(ClickWheelSimpleButton);
-    //     wheelSimpleBackButton.onClick.AddListener(ClickWheelSimpleBackButton);
-    //     wheelSimpleFrontButton.onClick.AddListener(ClickWheelSimpleFrontButton);
-    //     wheelTopButton.onClick.AddListener(ClickWheelTopButton);
-    //     wheelTopBackButton.onClick.AddListener(ClickWheelTopBackButton);
-    //     wheelTopFrontButton.onClick.AddListener(ClickWheelTopFrontButton);
-    //     #endregion
+        #region Wheel
+        wheelButton.onClick.AddListener(ClickWheelButton);
+        wheelSimpleButton.onClick.AddListener(ClickWheelSimpleButton);
+        wheelSimpleBackButton.onClick.AddListener(ClickWheelSimpleBackButton);
+        wheelSimpleFrontButton.onClick.AddListener(ClickWheelSimpleFrontButton);
+        wheelTopButton.onClick.AddListener(ClickWheelTopButton);
+        wheelTopBackButton.onClick.AddListener(ClickWheelTopBackButton);
+        wheelTopFrontButton.onClick.AddListener(ClickWheelTopFrontButton);
+        #endregion
 
-    //     removeButton.onClick.AddListener(ClickRemoveButton);
-    //     clearVehicleButton.onClick.AddListener(ClickClearVehicleButton);
-    //     playButton.onClick.AddListener(ClickPlayButton);
-    // }
+        removeButton.onClick.AddListener(ClickRemoveButton);
+        clearVehicleButton.onClick.AddListener(ClickClearVehicleButton);
+        playButton.onClick.AddListener(ClickPlayButton);
+    }
     public static void ClickStandardButton(){
         // engine.SetActive(false);
         // wheel.SetActive(false);
@@ -193,11 +193,12 @@ public class CanvasManagarEdit : MonoBehaviour
 
     public static void ClickPlayButton(){
         SceneManager.LoadScene(EditModeLevelController.instance.currentScene);
-        GameManager.instance.SetIsEditing(false);
-        GameManager.instance.SetCanCreate(false);
-        GameManager.instance.SetCanDestroy(false);
-        GameManager.instance.GetVehicle.GetComponent<Vehicle2>().Config();
-        GameManager.instance.GetVehicle.transform.position = new Vector3(1000,1000,1000);
+        EditModeLevelController.GoToGameScene();
+        // GameManager.instance.SetIsEditing(false);
+        // GameManager.instance.SetCanCreate(false);
+        // GameManager.instance.SetCanDestroy(false);
+        // GameManager.instance.GetVehicle.GetComponent<Vehicle2>().Config();
+        // GameManager.instance.GetVehicle.transform.position = new Vector3(1000,1000,1000);
         // FindObjectOfType<VehicleChanger>().VehicleObject = GameManager.instance.GetVehicle;
     }
 }
