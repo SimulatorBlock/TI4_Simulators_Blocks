24193b70cf764286e9e379d3d5e916e1fe6a6e6c
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2022-11-25 20:58:43 -0300

    Birds e tiles

--- Assets/Scripts/Fx/BirdFlockFx.cs
+++ Assets/Scripts/Fx/BirdFlockFx.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Fx
@@ -8,31 +9,45 @@ namespace Fx
 
         [Space]
         [Header("Movement")]
-        [SerializeField] [Range(0f, 2f)] private float speed = .6f;
+        [SerializeField] [Range(0f, 20f)] private float speed = 2f;
 
         [Header("Displacement")]
         [SerializeField] [Range(2f, 1f)] private float displacement = 1.5f;
         [SerializeField] [Range(0f, 1f)] private float speedDisplacement = .1f;
 
+        private Vector3 initialPos;
+        public bool canFly = false;
         private void Start()
         {
+            initialPos = transform.position;
             ResetPosition();
         }
-
+        
         private void Update()
         {
+            if (!canFly) return;
             _timePassed += Time.deltaTime * speedDisplacement;
 
             float timeInterpolation = Mathf.PingPong(_timePassed, 1f);
-            Vector3 direction = Vector3.Lerp(Vector3.left / displacement, Vector3.right / displacement, timeInterpolation) + Vector3.forward;
+            Vector3 direction =
+                Vector3.Lerp(Vector3.right / displacement, Vector3.right / displacement, timeInterpolation) +
+                Vector3.right;
             transform.position += direction * speed * Time.deltaTime;
 
-            if (transform.position.z >= 4f) ResetPosition();
+            //if (transform.position.z >= 4f) ResetPosition();
         }
 
-        private void ResetPosition()
+        IEnumerator Birds()
         {
-            transform.position = new Vector3(0, 0, -3f);
+            yield return new WaitForSeconds(2f);
+            canFly = true;
+        }
+
+        public void ResetPosition()
+        {
+            transform.position = initialPos;
+           
+            StartCoroutine(Birds());
         }
     }
 }
